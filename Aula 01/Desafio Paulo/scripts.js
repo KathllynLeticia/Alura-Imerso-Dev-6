5266f001fcefbe051d7b04157a2837cdb35fcdde
Kathllyn Santos <120657741+KathllynLeticia@users.noreply.github.com>
2023-02-10 19:09:48 -0300

    Delete Aula 01 directory

--- Aula 01/Desafio Paulo/scripts.js
+++
@@ -1,7 +0,0 @@
-var valorDoTether = 10;
-var cotacaoDoTether = 0.92;
-var nome = "Visitante";
-
-var valorEmEuro = valorDoTether * cotacaoDoTether;
-var valorEmEuro = valorEmEuro.toFixed(2);
-alert(`Olá ${nome}. O Tether em euro vale ${"€ " + valorEmEuro}`);
